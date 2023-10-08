1e24f32f41e9b8b27b6a4eee70693c0f5a11f804
meris <meris.bahti@gmail.com>
2023-10-08 08:56:09 +0200

    implement derive

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,11 +2,15 @@ module Lib
   ( someFunc,
     inc,
     add,
+    derive,
   )
 where
 
 someFunc :: IO ()
 someFunc = putStrLn "somefunk"
+
+derive :: Double -> (Double -> Double) -> (Double -> Double)
+derive dx f x = (f (x + (dx / 2)) - f (x - (dx / 2))) / dx
 
 add :: Int -> Int -> Int
 add = (+)
